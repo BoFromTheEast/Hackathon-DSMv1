7fd46248a40851e2429ba0785af87ee4c8209a79
bhoo <bhoo@iastate.edu>
2024-02-17 14:58:16 -0600

    need to pull

--- DSMv1/src/App.jsx
+++ DSMv1/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "./new.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,7 +14,7 @@ function App() {
     <>
       <div>
         <h1>Login</h1>
-        <a href="./first.html" /*target="_blank1"*/>
+        <a href="./first.jsx" /*target="_blank1"*/>
           <button>Click me!</button>
         </a>
 
