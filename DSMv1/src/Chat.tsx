a493a7e83fa942f10380cc777e289205580f2578
Aidan Thoma <aidan.thoma@drake.edu>
2024-02-17 17:41:48 -0600

    Added Bootstrap

--- DSMv1/src/Chat.tsx
+++ DSMv1/src/Chat.tsx
@@ -1,8 +1,10 @@
 //PascalCasing
 import React from "react";
+import {Button as BootstrapButton} from "react-bootstrap";
+
 function ChatPage() {
     return (
-        <h1>Heading</h1>
+        <BootstrapButton>Test button</BootstrapButton>
     )
 }
 export default ChatPage
