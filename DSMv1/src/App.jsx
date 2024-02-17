efc6e32bc1a6e35a5015d191440dc7d82df237fd
Charles <Cfarroyo@iastate.edu>
2024-02-17 15:05:07 -0600

    Test

--- DSMv1/src/App.jsx
+++ DSMv1/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import HomeScreen from "./homeScreen.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,10 +14,9 @@ function App() {
     <>
       <div>
         <h1>Login</h1>
-        <a href="./first.html" /*target="_blank1"*/>
+        <a href="./homeScreen.jsx" /*target="_blank1"*/>
           <button>Click me!</button>
         </a>
-
         <h3>Put your Username</h3>
         <input type="text" />
         <h3>Put your Password</h3>
