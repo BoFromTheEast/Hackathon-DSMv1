cfdd2c143e7c77645bc72b9c376c90dfa0c4fc38
bhoo <bhoo@iastate.edu>
2024-02-17 14:11:43 -0600

    html

--- DSMv1/src/App.jsx
+++ DSMv1/src/App.jsx
@@ -9,25 +9,14 @@ function App() {
   return (
     <>
       <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-        <h1>DSMv1</h1>
-        <a href="https://www.youtube.com/" target="button1">
+        <h1>Login</h1>
+        <a href="./first.html" /*target="_blank1"*/>
           <button>Click me!</button>
         </a>
+
+        <button >
+          
+        </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
