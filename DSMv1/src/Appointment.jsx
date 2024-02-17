4c3b669d5a17b51a7dbca4c5fb0a8b10a10cb7c9
Mason Sommers <Mason.sommers@drake.edu>
2024-02-17 14:54:59 -0600

    Appointment jsx file

---
+++ DSMv1/src/Appointment.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "Appointment./.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1><Day></Day></h1>
+        <a href="youtube.com" /*target="_blank1"*/>
+          <button>Click me!</button>
+        </a>
+
+        <button >
+          
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
