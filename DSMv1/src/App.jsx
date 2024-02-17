45b4ecb3331b4afdf5caa250d8243d91dee4f527
bhoo <bhoo@iastate.edu>
2024-02-17 15:35:07 -0600

    button working

--- DSMv1/src/App.jsx
+++ DSMv1/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import HomeScreen from "./homeScreen.jsx";
 
 // Import the necessary parts of React Router
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
